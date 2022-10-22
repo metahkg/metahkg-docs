89bcd0063301ed9b92b5a519ffe222b96d40b5b3
wcyat <wcyat@wcyat.me>
2022-10-22 16:38:31 +0800

    enable swagger features

--- src/components/swagger.tsx
+++ src/components/swagger.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import { Box } from "@mui/material";
 import { useColorMode } from "@docusaurus/theme-common";
@@ -30,11 +30,36 @@ export default function Swagger(props: { url: string }) {
                         ...(colorMode === "dark" && { fill: "white" }),
                     },
                     "& .version": {
-                        backgroundColor: "unset"
-                    }
+                        backgroundColor: "unset",
+                    },
                 }}
             >
-                <SwaggerUI url={url} />
+                <SwaggerUI
+                    url={url}
+                    displayOperationId
+                    deepLinking
+                    showExtensions
+                    requestSnippetsEnabled
+                    requestSnippets={{
+                        generators: {
+                            curl_bash: {
+                                title: "cURL (bash)",
+                                syntax: "bash",
+                            },
+                            curl_powershell: {
+                                title: "cURL (PowerShell)",
+                                syntax: "powershell",
+                            },
+                            curl_cmd: {
+                                title: "cURL (CMD)",
+                                syntax: "bash",
+                            }
+                        },
+                        defaultExpanded: true,
+                        languages: null,
+                        // e.g. only show curl bash = ["curl_bash"]
+                    }}
+                />
             </Box>
         </React.Fragment>
     );
