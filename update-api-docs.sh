f08ba11205215b9df4a15a050b8308177dc5f0a3
wcyat <wcyat@wcyat.me>
2022-11-06 16:11:07 +0800

    update api docs script

---
+++ update-api-docs.sh
@@ -0,0 +1,10 @@
+cd docs/api;
+lastitem=$(ls | tr '\n' ' ' | awk '{ print $(NF) }');
+lastitemdev=$(find *-dev -depth | tr '\n' ' ' | awk '{ print $(NF) }');
+if [ -d "$version" ]; then echo "version exists" && exit 0; fi;
+if [[ "$version" == *"-dev" ]];
+then
+  mv "$lastitemdev" "$version" && sed -i "s/${lastitemdev}/${version}/g" "${version}/index.mdx"
+else
+  cp -r "$lastitem" "$version" && sed -i "s/${lastitem}/${version}/g" "${version}/index.mdx"
+fi;
