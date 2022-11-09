3e6e6b68b439c395db1711e09b29d86291f1546f
wcyat <wcyat@wcyat.me>
2022-11-09 14:33:51 +0000

    Update update-api-docs.sh

--- update-api-docs.sh
+++ update-api-docs.sh
@@ -2,6 +2,8 @@ cd docs/api;
 lastitem=$(ls | tr '\n' ' ' | awk '{ print $(NF) }');
 lastitemdev=$(find *-dev -depth | tr '\n' ' ' | awk '{ print $(NF) }');
 echo version "$version"
+echo lastitem "$lastitem"
+echo lastitemdev "$lastitemdev"
 if [ -d "$version" ]; then echo "version exists" && exit 0; fi;
 if [[ "$version" == *"-dev" ]];
 then
