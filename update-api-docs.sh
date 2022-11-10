b160f74fe3423242aa2c1086011fd61d4dcead34
wcyat <wcyat@wcyat.me>
2022-11-10 11:42:28 +0000

    Update update-api-docs.sh

--- update-api-docs.sh
+++ update-api-docs.sh
@@ -8,7 +8,8 @@ echo lastitemdev "$lastitemdev"
 if [ -d "$version" ]; then echo "version exists" && exit 0; fi;
 if [[ "$version" == *"-dev" ]];
 then
-  mv "$lastitemdev" "$version" && sed -i "s/${lastitemdev}/${version}/g" "${version}/index.mdx"
+  mv "$lastitemdev" "$version" && sed -i "s/${lastitemdev}/${version}/g" "${version}/index.mdx";
+  echo "/docs/api/${lastitemdev} /docs/api/${version} 301" >> ../../_redirects;
 else
-  cp -r "$lastitem" "$version" && sed -i "s/${lastitem}/${version}/g" "${version}/index.mdx"
+  cp -r "$lastitem" "$version" && sed -i "s/${lastitem}/${version}/g" "${version}/index.mdx";
 fi;
